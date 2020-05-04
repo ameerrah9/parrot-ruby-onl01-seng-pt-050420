9f9c4821d595a55d9dad69502d0197134ab3a000
Ghameerah McCullers <mccullers.ghameerah@gmail.com>
2020-05-04 21:01:36 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(a="Squawk!")
-  puts #{a}
-  return #{a}
+def parrot(name="Squawk!")
+  puts #{name}
+  return #{name}
 end
